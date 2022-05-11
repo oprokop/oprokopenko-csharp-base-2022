17553b67423ceb8818f3d360af5418173f7424ff
oprokop <olechkakireeva@gmail.com>
2022-05-11 19:00:41 +0300

    Commit for task2

---
+++ 1-2-base-types/ArithmeticApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace typesApp;
+internal class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input x");
+        float x = float.Parse(Console.ReadLine());
+        Console.WriteLine("Input y");
+        float y = float.Parse(Console.ReadLine());
+        Console.WriteLine("Input z");
+        float z = float.Parse(Console.ReadLine());
+
+        float f = (x + y + z) * (x - z) / (y * y) + 2 * z;
+        Console.WriteLine($"F = {f.ToString("0.00")}");
+    }
+
+}
