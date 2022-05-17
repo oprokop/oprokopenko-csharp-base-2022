e194b4cb92f9b382ad160a069fd1f7c581ec6cb5
oprokop <olechkakireeva@gmail.com>
2022-05-18 02:04:23 +0300

    Commit for Task2 from Homework1-3

--- 1-3-operators/OperatorsHomework/Task2/Program.cs
+++ 1-3-operators/OperatorsHomework/Task2/Program.cs
@@ -7,24 +7,28 @@ internal class Program
     {
         while (true)
         {
-            bool n;
-            Console.WriteLine("Сhoose option: \n 1. Enter count of array elements \n 2. Show array: in line");
+            Console.WriteLine("\nСhoose option: \n 1. Enter array \n 2. Show array: in line");
             Console.WriteLine(" 3. Bubble sort \n 4. Sum of all elements \n 5. Multiplexing of all elements");
             Console.WriteLine(" 6. The biggest and the lowest elements \n 7. Reverse array \n 8. Add element by index");
             Console.WriteLine(" 9. Remove elements by index \n Or just type <exit>");
             switch (Console.ReadLine())
-
             {
                 case "1":
-                    n = int.TryParse(Console.ReadLine(), out int i);
-                    if (n == true)
+                    Console.WriteLine("Enter count of array elements");
+                    if (int.TryParse(Console.ReadLine(), out int i) && i > 0)
                     {
-                        int[] mas = new int[i];
+                        Console.WriteLine("Enter numbers one by one:");
+                        MassEnter(i);
                     }
                     else
                     {
                         Console.WriteLine("Invalid data");
+                        break;
                     }
+                    break;
+                case "2":
+                    Console.WriteLine("Your massive is ");
+                    MassShow(i);
                     break;
             }
 
@@ -34,4 +38,24 @@ internal class Program
         }
 
     }
+    public static void MassEnter(int i)
+    {
+        int[] mas = new int[i];
+        for (int n = 0; n < mas.Length; n++)
+        {
+            Console.WriteLine($"Enter {n} element of array");
+            mas[n] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Now choose the array action (2-9)");
+    }
+    public static void MassShow(int i)
+    {
+        for (int n = 0; n < i; n++)
+        {
+            Console.Write(mas[n]);
+        }
+    }
+
 }
+
+
