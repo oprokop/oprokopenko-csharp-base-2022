f15273c32201cc4cd669c6da57e86295984a1141
oprokop <olechkakireeva@gmail.com>
2022-05-26 01:18:50 +0300

    Commit1 for Homework1-5 (converting to binary numeral system)

---
+++ 1-5-classes/CalculatingApp/CalculatingApp/Converters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingApp
+{
+    public class Converters
+    {
+        public static void ToBinary(int number)
+        {
+            int n = 0;
+            int rest;
+            for (int number1 = number; number1 >= 2;)
+            {
+                number1 /= 2;
+                n++;
+                if (number1 == 2)
+                {
+                    n++;
+                }
+            }
+            int[] mas = new int[n];
+            for (int j = n - 1; j >= 0; j--)
+            {
+
+                if (j == 0)
+                {
+                    mas[0] = 1;
+                }
+                else
+                {
+                    rest = number % 2;
+                    mas[j] = rest;
+                    number /= 2;
+                }
+            }
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(mas[j]);
+            }
+            Console.WriteLine();
+        }
+        public static string ToHex(int number)
+        {
+
+            return Convert.ToString(number);
+        }
+        public static string ToOct(int number)
+        {
+
+            return Convert.ToString(number);
+        }
+        public static void Dividing()
+        {
+
+        }
+    }
+}
---
+++ 1-5-classes/CalculatingApp/CalculatingApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculatingApp;
+internal class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter the number:");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine("Choose numeral system: bin, hex or oct");
+                switch(Console.ReadLine())
+                {
+                    case "bin":
+                    case "ишт":
+                    case "2":
+                        Converters.ToBinary(number);
+                        break;
+                    case "hex":
+                        //Сonverters.ToHex(number);
+                        break;
+                    case "oct":
+                        //converters.ToOct();
+                        break;
+                    case "exit":
+                        return;
+                    default:
+                        Console.WriteLine("Such name doesn't exist, try again!");
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid data. Try again...");
+            }
+            
+        }
+    }
+}
