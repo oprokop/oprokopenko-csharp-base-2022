18aab0762c6fadb7a10b0f72b6e54b0a6a596666
oprokop <olechkakireeva@gmail.com>
2022-07-14 17:18:56 +0300

    Commit for Task1-12 (4 methods in FileAction class)

---
+++ 1-12-files/FileApp/FileAction.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileApp
+{
+    public static class FileAction
+    {
+        private static string FilePath = @"D:\For_HW1-12";
+
+        public static void Write<T>(string fileName, T data)
+            where T : class
+        {
+            string FullPath = $@"{FilePath}/{fileName}";
+
+            using (var file = File.Create(FullPath))
+            {
+                using (var writer = new StreamWriter(file))
+                {
+                    writer.Write(data);
+                }
+            }
+        }
+
+        public static T Read<T>(string fileName, Func<string, T> createInstance)
+            where T : class
+        {
+            string FullPath = $@"{FilePath}/{fileName}";
+
+
+            if (File.Exists(FullPath))
+            {
+                using (var reader = new StreamReader(File.Open(FullPath, FileMode.Open)))
+                {
+                    var line = reader.ReadLine();
+
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        return createInstance(line);
+                    }
+                    else return null;
+                }
+            }
+            else return null;
+        }
+
+        public static void Write<T>(string fileName, List<T> data)
+            where T: class
+        {
+            string FullPath = $@"{FilePath}/{fileName}";
+
+            using (var file = File.Create(FullPath))
+            {
+                using (var writer = new StreamWriter(file))
+                {
+                    foreach(var item in data)
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        public static List<T> ReadMany<T>(string fileName, Func<string, T> createInstance)
+            where T: class
+        {
+            string FullPath = $@"{FilePath}/{fileName}";
+
+            if (File.Exists(FullPath))
+            {
+                var listOfInstances = new List<T>();
+
+                using (var reader = new StreamReader(File.Open(FullPath, FileMode.Open)))
+                {
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                            break;
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            var instance = createInstance(line);
+
+                            listOfInstances.Add(instance);
+                        }
+                    }
+                }
+                return listOfInstances;
+            }
+            else return null;
+        }
+    }
+}
---
+++ 1-12-files/FileApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FileApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDeposit = new Deposit 
+            { 
+                Name = "Deposit", 
+                Duration = 12, 
+                PercentProfit = 7.5 
+            };
+
+            var deposits = new List<Deposit>
+            {
+                new Deposit{Name = "LongDeposit", Duration = 12, PercentProfit = 6.9 },
+                new Deposit{Name = "ShortDeposit", Duration = 3, PercentProfit = 5.9 },
+                new Deposit{Name = "IrrevocableLongDeposit", Duration = 12, PercentProfit = 7.5, IsMonthlyCapitalized = true},
+                new Deposit{Name = "IrrevocableShortDeposit", Duration = 3, PercentProfit = 6.5, IsMonthlyCapitalized = true},
+                new Deposit{Name = "SuperLongDeposit", Duration = 24, PercentProfit = 7.9 },
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Choose action:");
+                Console.WriteLine("1.to write single object into file");
+                Console.WriteLine("2.to read single object from file");
+                Console.WriteLine("3.to write collection of objects into file");
+                Console.WriteLine("4.to read collection of objects from file");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+
+                        Console.WriteLine("Enter the name of file in accordance to format CSV");
+
+                        Regex regexToWrite = new(@"[A-Z]?.csv");
+
+                        var fileName = Console.ReadLine();
+
+                        if (regexToWrite.IsMatch(fileName))
+                        {
+                            FileAction.Write(fileName, myDeposit);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong file name, try again!");
+                            break;
+                        }
+                        break;
+
+                    case "2":
+
+                        Console.WriteLine("Enter the name of file in accordance to format CSV");
+                        
+                        Regex regexToRead = new (@"[A-Z]?.csv");
+
+                        var fileNameToRead = Console.ReadLine();
+
+                        if (regexToRead.IsMatch(fileNameToRead))
+                        {
+                            Console.WriteLine(FileAction.Read(fileNameToRead, Deposit.FromString));
+                        }
+                        
+                        break;
+
+                    case "3":
+
+                        FileAction.Write("list.csv", deposits);
+
+                        break;
+
+                    case "4":
+
+                        var result = FileAction.ReadMany("list.csv", Deposit.FromString);
+                        
+                        foreach(var item in result)
+                        {
+                            Console.WriteLine(item);
+                        }
+                       
+                        break;
+
+                    case "exit":
+
+                        return;
+
+                }
+            }
+        }
+    }
+}
+
